b11e8cefb5efd332a680f039fd9cb0cd6c79a711
Jean Joeris <joeris.peter@gmail.com>
2016-08-08 17:39:54 -0600

    final commit: guesser is completed, including conditional styling

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-rand_num = rand(101)
+SECRET_NUMBER = rand(101)
+
+def check_guess(guess)
+  return ["Way too high!", "way_high"] if guess > SECRET_NUMBER + 5
+  return ["Way too low!", "way_low"] if guess < SECRET_NUMBER - 5
+  return ["Too high!", "high"] if guess > SECRET_NUMBER
+  return ["Too low!", "low"] if guess < SECRET_NUMBER
+  return ["You got it right!", "correct"] if guess == SECRET_NUMBER
+end
+
 get '/' do
-   erb :index, locals: {number: rand_num}
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, locals: {number: SECRET_NUMBER, message: message[0], message_style: message[1]}
 end
